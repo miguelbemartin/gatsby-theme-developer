d054f46a8e8c57802f0bc392c6da7001d6c00735
Miguel Ángel Martín <miguelbemartin@gmail.com>
2022-01-14 23:33:00 +0100

    Fix initial script

--- gatsby-node.js
+++ gatsby-node.js
@@ -313,7 +313,21 @@ Curabitur bibendum tincidunt tellus sit amet viverra. Etiam ac tellus suscipit, 
 
 `;
 
+    const pageIndex = `---
+slug: /
+title: The Homepage
+template: page
+coverImage: ""
+---
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu lorem eget metus venenatis dignissim. Vivamus lobortis, mauris eu facilisis sodales, nisl metus ultrices orci, non finibus quam nulla ac est. Fusce egestas faucibus lorem quis varius. Morbi id faucibus tellus, sit amet venenatis metus. Vivamus diam odio, mollis ac varius ut, imperdiet ut nunc. Sed sagittis nulla et tempus egestas. Donec sit amet molestie turpis. Maecenas purus ante, rutrum id erat eu, rutrum ultrices ligula.
+
+Curabitur bibendum tincidunt tellus sit amet viverra. Etiam ac tellus suscipit, fringilla dui quis, fringilla nisi. Proin mattis ultrices orci id mollis. Cras laoreet nisl est, at rutrum augue mollis nec. In scelerisque orci enim, quis mattis risus tristique sed. Vivamus venenatis auctor nunc, ut auctor mi ornare vitae. Curabitur viverra aliquam tempor. Donec a urna nec nunc sagittis consequat. Phasellus non risus non erat vestibulum pharetra eget in massa. Vestibulum venenatis dui nec elit molestie, a consequat nunc blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam libero dolor, vestibulum consectetur nunc id, porta rhoncus tortor. Mauris tincidunt ornare dui in iaculis.
+
+`;
+
     fs.writeFileSync(path.join(contentDirectory, "2020-11-20-hello-world.md"), helloWorldArticle);
     fs.writeFileSync(path.join(contentDirectory, "page-about.md"), pageAbout);
+    fs.writeFileSync(path.join(contentDirectory, "page-index.md"), pageIndex);
 
 }
